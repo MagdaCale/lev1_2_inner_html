6b7bd5f929de663a296e589ad85281e2dd4b4570
Magdalena Cale <107847566+MagdaCale@users.noreply.github.com>
2022-07-15 11:38:41 +0200

    innerHTML

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+console.log("Hallo Heat")
+
+
+let info = document.getElementById('info')
+console.log(info)
+info.innerHTML = '<h1>Hello World</h1>'
+info.innerHTML += '<h2>How are you?</h2>'
+
+//document.querySelector('#info').innerHTML = 'Hallo Welt!'
+//info.innerHTML = '<h2>How are you?</h2>'
+
+let container = document.getElementById('container')
+console.log(container)
+container.innerHTML = '<p>start of the element</p>'
+
+
+document.write("end of the element")
